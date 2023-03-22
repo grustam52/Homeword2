83789d1bdc82a93b501967ff599af3fe5a8d1284
grustam52 <grustam52@gmail.com>
2023-03-22 19:08:02 +0300

    Домашнее задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         task1();
         task2();
         task3();
         task4();
         task5();
         task6();
+        task7();
+        task8();
 
 
     }
+
     public static void task1() {
         System.out.println("Задача 1");
         var dog = 8.0;
@@ -18,6 +21,7 @@ public class Main {
         var paper = 763789;
         System.out.println(paper);
     }
+
     public static void task2() {
         System.out.println("Задача 2");
         var dog = 8.0;
@@ -30,6 +34,7 @@ public class Main {
         paper = paper + 4;
         System.out.println(paper);
     }
+
     public static void task3() {
         System.out.println("Задача 3");
         var dog = 8.0;
@@ -45,6 +50,7 @@ public class Main {
         paper = paper - 7639;
         System.out.println(paper);
     }
+
     public static void task4() {
         System.out.println("Задача 4");
         var friend = 19;
@@ -54,6 +60,7 @@ public class Main {
         friend = friend / 7;
         System.out.println(friend);
     }
+
     public static void task5() {
         System.out.println("Задача 5");
         var frog = 3.5;
@@ -65,7 +72,9 @@ public class Main {
         frog = frog + 4;
         System.out.println(frog);
     }
-        public static void task6() {
+
+    public static void task6() {
+        System.out.println("Задача 6");
         var boxer1Weight = 78.2;
         var boxer2Weight = 82.7;
         var boxersWeight = boxer1Weight + boxer2Weight;
@@ -73,4 +82,28 @@ public class Main {
         var differenceWeight = boxer2Weight - boxer1Weight;
         System.out.println("Разница веса боксеров " + differenceWeight + " кг");
     }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+        var boxer1Weight = 78.2;
+        var boxer2Weight = 82.7;
+        var differenceWeight = boxer2Weight - boxer1Weight;
+        System.out.println("Разница веса боксеров " + differenceWeight + " кг");
+        var differenceWeight1 = boxer2Weight % boxer1Weight;
+        System.out.println("Разница веса боксеров " + differenceWeight1 + " кг!");
+    }
+
+    public static void task8() {
+        System.out.println("Задача 8");
+        var mounthHours = 640;
+        var dayHours = 8;
+        var companyMember = mounthHours /  dayHours;
+        System.out.println("Всего работников в компании " + companyMember + " человек");
+        var member = companyMember + 94;
+        var hours = member * dayHours ;
+        System.out.println("Если в компании работает " + member + " человек, то всего "
+        + hours + " часов работы может быть поделено между сотрудниками");
+    }
+
+
 }
